2280f46f2a026c300015b185cd8cc34a36e6949a
Shine-wpf <1596989740@qq.com>
2021-09-28 22:14:10 +0800

    [Add]商品列表展示

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -5,14 +5,26 @@
     </nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <recommonend-view :recommends="recommends"></recommonend-view>
+    <feature-view />
+    <tab-control
+      class="tab-control"
+      :titles="titles"
+      @tabClick="tabClick"
+    ></tab-control>
+    <goods-list :goods="showGoods" />
   </div>
 </template>
 
 <script>
-import NavBar from "components/common/navbar/NavBar";
 import HomeSwiper from "./childComps/HomeSwiper";
 import RecommonendView from "./childComps/RecommendView";
-import { getHomeMultidata } from "api/home";
+import FeatureView from "./childComps/FeatureView";
+
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl";
+import GoodsList from "components/content/goods/GoodsList";
+
+import { getHomeMultidata, getHomeGoods } from "api/home";
 
 export default {
   name: "Home",
@@ -20,26 +32,89 @@ export default {
     return {
       banners: [],
       recommends: [],
+      titles: ["流行", "新款", "精选"],
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] },
+      },
+      currentType: "pop",
     };
   },
   components: {
     NavBar,
     HomeSwiper,
     RecommonendView,
+    FeatureView,
+    TabControl,
+    GoodsList,
   },
   created() {
     // 请求多个数据
-    getHomeMultidata().then((res) => {
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    });
+    this.getHomeMultidata();
+    // 请求商品数据
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
+  },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list;
+    },
+  },
+  methods: {
+    /**
+     * 事件监听相关的方法
+     */
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = "pop";
+          break;
+        case 1:
+          this.currentType = "new";
+          break;
+        case 2:
+          this.currentType = "sell";
+          break;
+      }
+    },
+    /**
+     * 网络请求相关的方法
+     */
+    getHomeMultidata() {
+      getHomeMultidata().then((res) => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then((res) => {
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+      });
+    },
   },
 };
 </script>
 
 <style>
+#home {
+  padding-top: 44px;
+}
 .nav-home {
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
   background-color: var(--color-tint);
   color: #fff;
+  z-index: 1000;
+}
+.tab-control {
+  position: sticky;
+  top: 44px;
+  z-index: 1000;
 }
 </style>
